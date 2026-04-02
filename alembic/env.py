bc1afd1494b0abce0a65996a142084cc1f672336
lucas alonso <lucasjosealonso@gmail.com>
2026-04-02 12:30:56 -0300

    fix: env py error

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,6 @@ from logging.config import fileConfig
 from alembic import context
 from app.core.config import settings
 from app.models.base import Base
-from alembic.runtime.migration import MigrationContext
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
